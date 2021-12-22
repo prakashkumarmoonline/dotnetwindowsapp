5af6e2a4c3d6caaa86539cee1a0115d1171230df
prakashkumarmoonline <95124965+prakashkumarmoonline@users.noreply.github.com>
2021-12-23 02:24:33 +0530

    dotnew windows application

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi Team!");
+Console.WriteLine("This is Dotnet Windows Console Application");
+Console.WriteLine("All the Best!");
